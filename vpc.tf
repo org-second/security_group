a1ac37cd8c711b638af1d375a714a30ccb3b554d
hn <110962615+hngithubrepo@users.noreply.github.com>
2023-04-11 19:35:53 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,14 @@
- module "sportgully_vpc" {
-   source = "./provider/aws/vpc/v1"
-   cidr_block = "10.0.0.0/16"
-   tags = {
-      name : "sportgully",
-      Author : "Terraform",
-      Contact : var.Contact,
-      Environment : var.Environment,
-   }
-}
+ module "sportgully_rds_security_group" {
+   source = "./security_group"
+   name = "sportgully_security_group"
+   vpc_id = module.sportgully_vpc.vpc_id
+   description = "Private connectivity to RDS DBs"
+   ingress_rules = [
+    {
+      from = 3306
+      to = 3306
+      protocol = "TCP"
+      cidr_block = "10.0.0.0/16"
+    } 
+   ]  
+   
