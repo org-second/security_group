8985124466fc6d5ad0612e6803ba582b5cf80784
hn <110962615+hngithubrepo@users.noreply.github.com>
2023-04-11 20:49:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
- provider "aws" {
-     sharedcredentials_file = "/aws/credentials"
-     profile = "default"
-     region = var.aws_region
+provider "aws" {
+     region = "ap-south-1"
+     access_key = "AKIASZWQXZBLGF4HSLGW"
+     secret_key = "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"
+     
  }
